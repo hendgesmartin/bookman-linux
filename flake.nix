ac54a4bf1c63d91516ac29b140a21eac6d13765b
Carsten Csiky <carsten.csiky@vier.ai>
2024-08-30 19:38:43 +0200

    Update to 1.16

--- flake.nix
+++ flake.nix
@@ -1,15 +1,11 @@
 {
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
-
-  # inputs.bookmansrc.url = "./bookman.exe";
-  # inputs.bookmansrc.flake = false;
-
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
 
   inputs.install4j.url = "https://maven.ej-technologies.com/repository/com/install4j/install4j-runtime/10.0.8/install4j-runtime-10.0.8.jar";
   inputs.install4j.flake = false;
 
-  inputs.bookman-windows-download.url = "https://cockpit.bookman-gmbh.de/api/java/update/newest/WINDOWS/file"; # 1.15.0
+  inputs.bookman-windows-download.url = "https://cockpit.bookman-gmbh.de/api/java/update/newest/WINDOWS/file"; # 1.16
   inputs.bookman-windows-download.flake = false;
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
@@ -20,7 +16,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
+        jdk = pkgs.jdk21.override { enableJavaFX = true; };
       in
       rec {
         packages.default = pkgs.writeShellApplication
@@ -29,16 +25,16 @@
             runtimeInputs = [ pkgs.coreutils pkgs.xdg-utils  ];
             text = ''
               mkdir -p "$HOME/.config/bookman"
-              "${pkgs.jdk}/bin/java" \
+              "${jdk}/bin/java" \
                   -Duser.home="$HOME/.config/bookman" \
                   -Dde.bookman.deployMode=LIVE \
                   -Dde.bookman.javaBackendUri=https://cockpit.bookman-gmbh.de/api/java/ \
                   -Dde.bookman.microsoftApplicationId=a7719d9a-1877-4bd1-a3c7-e3f8edf86485 \
                   -Dde.bookman.microsoftSsoRedirectUrl=http://localhost:4826 \
-                  --add-modules ALL-MODULE-PATH \
+                  --add-modules javafx.swing,javafx.graphics,javafx.fxml,javafx.media,javafx.controls \
                   --add-exports javafx.base/com.sun.javafx.event=org.controlsfx.controls \
                   --add-opens javafx.base/com.sun.javafx.event=ALL-UNNAMED \
-                  -cp "${packages.bookman-overrides}:${pkgs.jdk}/lib/*:${packages.bookman-jars}/*" \
+                  -cp "${packages.bookman-overrides}:${jdk}/lib/*:${packages.bookman-jars}/*" \
                   de.bookman.start.MainGradle
             '';
           };
@@ -48,7 +44,7 @@
             src = ./override;
           } ''
           mkdir -p $out
-          find $src -type f -name '*.java' -exec ${pkgs.jdk}/bin/javac -d $out  '{}' \;
+          find $src -type f -name '*.java' -exec ${jdk}/bin/javac -d $out  '{}' \;
         '';
 
 
